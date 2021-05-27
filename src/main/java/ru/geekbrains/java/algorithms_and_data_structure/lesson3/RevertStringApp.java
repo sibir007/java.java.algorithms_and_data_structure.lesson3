e3f91d201f4fb021c6ad1975cc714b2ac2fe7d1e
sibir007 <sibiriakoff2006@yandex.ru>
2021-05-28 00:44:40 +0300

    Change  RevertStringApp

--- src/main/java/ru/geekbrains/java/algorithms_and_data_structure/lesson3/RevertStringApp.java
+++ src/main/java/ru/geekbrains/java/algorithms_and_data_structure/lesson3/RevertStringApp.java
@@ -1,11 +1,35 @@
 package ru.geekbrains.java.algorithms_and_data_structure.lesson3;
 
+import ru.geekbrains.java.algorithms_and_data_structure.lesson3.stack.MyArrayListStack;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RevertStringApp {
-    public static void run() {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void run(){
+       // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        InputStreamReader in = new InputStreamReader(System.in);
+        MyArrayListStack stack = new MyArrayListStack();
 
+        try {
+        while (true) {
+            int i;
+            while (true) {
+                if (!((i = in.read()) != -1)) break;
+
+                stack.push(i);
+            }
+            while (!stack.isEmpty()){
+                System.out.print(stack.pop());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        RevertStringApp.run();
     }
 }
